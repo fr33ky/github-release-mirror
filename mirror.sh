8702b15c46e06b996ea7d8a5f52b4435d59db3e3
fr33ky <38321094+fr33ky@users.noreply.github.com>
2023-03-15 20:02:59 +0100

    [feat] Add a match for releases

--- mirror.sh
+++ mirror.sh
@@ -1,27 +1,36 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 REPO=${1}
-MIRROR_DIR="/mirror"
+if [ -z ${2+x} ]; then MATCH="."; else MATCH="${2}"; fi
+
+readonly MIRROR_DIR="./mirror"
 SRC_URL="https://api.github.com/repos/${REPO}/releases"
-DEST="${MIRROR_DIR}/${REPO}"
 
-RELEASES=$(curl ${SRC_URL} | jq '[.[] | {tag_name: .tag_name, draft: .draft, assets: [.assets[].browser_download_url]}]')
+RELEASES=$(curl --silent "${SRC_URL}" | jq '[.[] | {tag_name: .tag_name, draft: .draft, assets: [.assets[].browser_download_url]}]')
 
-for RELEASE in $(echo ${RELEASES} | jq -r '.[] | @base64'); do
-  DRAFT=$(echo ${RELEASE} | base64 --decode | jq -r '.draft')
-  NAME=$(echo ${RELEASE} | base64 --decode | jq -r '.tag_name')
-  URLS=$(echo ${RELEASE} | base64 --decode | jq -r '.assets | .[]')
+for RELEASE in $(echo "${RELEASES}" | jq -r '.[] | @base64'); do
+  DRAFT=$(echo "${RELEASE}" | base64 --decode | jq -r '.draft')
+  NAME=$(echo "${RELEASE}" | base64 --decode | jq -r '.tag_name')
+  URLS=$(echo "${RELEASE}" | base64 --decode | jq -r '.assets | .[]')
 
   if [[ "${DRAFT}" != "false" ]]; then
     continue
   fi
 
-  RELEASE_DEST=${DEST}/${NAME}
-  if [[ -d ${RELEASE_DEST} ]]; then
-    continue
+  RELEASE_DEST="${MIRROR_DIR}/${REPO}/${NAME}"
+  MATCHED_URLS=""
+  for url in ${URLS}; do
+    if [[ ${url} =~ ${MATCH} ]]; then
+      MATCHED_URLS="${MATCHED_URLS} ${url}"
+    fi
+  done
+  # Trim left (because of first assignation
+  MATCHED_URLS="${MATCHED_URLS##*( )}"
+
+  if [ -n "${MATCHED_URLS}" ]; then
+    mkdir -p "${RELEASE_DEST}"
+    # Do not double quote MATCHED_URLS
+    # shellcheck disable=SC2086
+    wget --timestamping --directory-prefix="${RELEASE_DEST}" --no-verbose ${MATCHED_URLS}
   fi
-
-  mkdir -p ${RELEASE_DEST}
-  wget -P ${RELEASE_DEST} --no-verbose ${URLS}
-
 done
